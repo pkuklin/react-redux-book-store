9ee3076630d73f04b6b31739f2b8e79209e862fe
pkuklin <kuklinpavlo@gmail.com>
2017-06-26 10:48:47 +0300

    Done

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'Javascript', pages: 101},
+    {title: 'Ruby', pages: 51},
+    {title: 'Java', pages: 222},
+    {title: 'SQL', pages: 345},
+  ]
+}
